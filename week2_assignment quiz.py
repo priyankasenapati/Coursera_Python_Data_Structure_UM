23051b5469ffb1db4ddab686d498a61f99690970
Priyanka Senapati <write2priyankasenapati@gmail.com>
2021-01-03 14:31:29 +0530

    Update week2_assignment quiz.py

--- week2_assignment quiz.py
+++ week2_assignment quiz.py
@@ -1,6 +1,4 @@
-# You can write any code you like in the window below.
-# There are three files loaded and ready for you to open
-# if you want to do file processing: "mbox-short.txt", "romeo.txt", and "words.txt"
+# You can write any code you like in the window below.There are three files loaded and ready for you to open if you want to do file processing: "mbox-short.txt", "romeo.txt", and "words.txt"
 
 
 
