b2883a02a9a45b90363eab63d7daf3443c153886
Priyanka Senapati <write2priyankasenapati@gmail.com>
2021-01-03 14:30:49 +0530

    Add files via upload

---
+++ week2_assignment quiz.py
@@ -0,0 +1,26 @@
+# You can write any code you like in the window below.
+# There are three files loaded and ready for you to open
+# if you want to do file processing: "mbox-short.txt", "romeo.txt", and "words.txt"
+
+
+
+fh = open("mbox-short.txt","r")
+count = 0
+for line in fh:
+    print(line.strip())
+    count = count + 1
+print(count,"Lines")
+
+gh = open("romeo.txt","r")
+count = 0
+for line in gh:
+    print(line.strip())
+    count = count + 1
+print(count,"Lines")
+
+kh = open("words.txt","r")
+count = 0
+for line in kh:
+    print(line.strip())
+    count = count + 1
+print(count,"Lines")
